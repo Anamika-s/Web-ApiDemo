4082e4b957245a40f462631a7b44ac2831a04de1
Anamika-s <anamika.sawhney22@gmail.com>
2023-05-30 10:51:29 +0530

    client

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,73 @@
+﻿using ConsoleClient.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace ConsoleClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             GetStudents().Wait();
+            GetStudentById(2).Wait();
+        }
+        static async Task  GetStudents()
+        {
+            using (var client = new HttpClient())
+            {
+                //Send HTTP requests from here. 
+                client.BaseAddress = new Uri("http://localhost:5259/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method  
+                HttpResponseMessage response = await client.GetAsync("api/Student");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = response.Content.ReadAsStringAsync();
+                    jsonString.Wait();
+                    var student = JsonConvert.DeserializeObject<List<Student>>(jsonString.Result);
+
+                    foreach (var temp in student)
+                    {
+                        Console.WriteLine("Id:{0}\tName:{1}", temp.Id, temp.Name);
+                        //  Console.WriteLine("No of Employee in Department: {0}", department.Employees.Count);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(response.ReasonPhrase);
+                    Console.WriteLine("Internal server Error");
+                }
+
+            }
+
+
+        }
+
+        static async Task GetStudentById(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                //Send HTTP requests from here. 
+                client.BaseAddress = new Uri("http://localhost:5259/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method  
+                HttpResponseMessage response = await client.GetAsync("api/Student/"+id);
+                if (response.IsSuccessStatusCode)
+                {
+                    Student student = await response.Content.ReadAsAsync<Student>();
+                    Console.WriteLine("Id:{0}\tName:{1}", student.Id, student.Name);
+                    //  Console.WriteLine("No of Employee in Department: {0}", department.Employees.Count);
+                }
+                else
+                {
+                    Console.WriteLine(response.ReasonPhrase);
+                    Console.WriteLine("Internal server Error");
+                }
+
+            }
+        }
+
+    }
+}
---
+++ MVCClient/Controllers/StudentController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCClient.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace MVCClient.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: StudentController
+        public async Task<ActionResult> Index()
+        {
+            using (var client = new HttpClient())
+            {
+                List<Student> students = new List<Student>();
+                //Send HTTP requests from here. 
+                client.BaseAddress = new Uri("http://localhost:5259/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method  
+                HttpResponseMessage response = await client.GetAsync("api/Student");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = response.Content.ReadAsStringAsync();
+                    jsonString.Wait();
+                    students = JsonConvert.DeserializeObject<List<Student>>(jsonString.Result);
+
+                }
+                else
+                {
+                   ViewBag.msg = response.ReasonPhrase;
+                    
+                }
+                return View(students);
+
+            }
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
